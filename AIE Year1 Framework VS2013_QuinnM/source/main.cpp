daad40938016d3509ac14f70dcea265cb40ee00e
QuinnJMosher <Darkstaro59@gmail.com>
2014-09-15 15:28:05 -0700

    9/15/14

--- AIE Year1 Framework VS2013_QuinnM/source/main.cpp
+++ AIE Year1 Framework VS2013_QuinnM/source/main.cpp
@@ -1,100 +1,65 @@
 ﻿#include "AIE.h"
 #include <iostream>
 
+//constant vars
+static int SCREEN_MAX_X = 900, SCREEN_MAX_Y = 600;
+
+struct Player {
+	unsigned int spriteID;
+
+	unsigned int score;
+	unsigned int hitPoints;
+
+	float posX;
+	float posY;
+
+	float velX;
+	float velY;
+
+};
+
+struct Point {
+	float x, y;
+};
+
+struct Rectangle {
+	Point a, b;
+	SColour color;
+
+	float getWidth() {
+		return abs(a.x - b.x);
+	}
+
+	float getHeight() {
+		return abs(a.y - b.y);
+	}
+
+	void draw() {
+		int tempSprite = CreateSprite("./images/crate_sideup.png", getHeight(), getWidth(), true, color);
+		MoveSprite(tempSprite, b.x - a.x, b.y - a.y);
+		DrawSprite(tempSprite);
+		DestroySprite(tempSprite);
+	}
+
+};
+
 int main( int argc, char* argv[] )
 {	
-	//constant vars
-	static int SCREEN_MAX_X = 1200, SCREEN_MAX_Y = 700;
-	static float SPEED = 0.5f;
-
 	Initialise(SCREEN_MAX_X, SCREEN_MAX_Y, false, "My Awesome Game");
     
     SetBackgroundColour(SColour(0, 0, 0, 255));
 
-	int img1 = CreateSprite("./images/invaders/invaders_1_00.png", 64, 64, true);
-	int img2 = CreateSprite("./images/invaders/invaders_1_01.png", 64, 64, true);
-	int myTextureHandle = img1;
-
-    float xPos = 100;
-    float yPos = 100;
-
-	float time = 0;
-	float interval = 0.8f;
-
-	//direction vars
-	bool movingRight = true;
-	bool movingUp = true;
-
-	//key vars	
-	bool xKeyDown = false;
-	bool yKeyDown = false;
+	Rectangle rect;
+	rect.a.x = 5;
+	rect.a.y = 10;
+	rect.b.x = 200;
+	rect.b.y = 200;
+	rect.color = SColour(255, 0, 0, 255);
 
     //Game Loop
     do
 	{
-		time += GetDeltaTime();
-		if (time > interval) {
-			time -= interval;
-			if (myTextureHandle == img1) {
-				myTextureHandle = img2;
-			} else {
-				myTextureHandle = img1;
-			}
-		}
-
-		//movement code
-		if (movingRight) {
-			xPos += SPEED;
-			if (xPos > SCREEN_MAX_X) {
-				movingRight = !movingRight;
-				xPos = SCREEN_MAX_X;
-			}
-		}
-		else 
-		{
-			xPos -= SPEED;
-			if (xPos < 0) {
-				movingRight = !movingRight;
-				xPos = 0;
-			}
-		}
-
-		if (movingUp) {
-			yPos += SPEED;
-			if (yPos > SCREEN_MAX_Y) {
-				movingUp = !movingUp;
-			}
-		}
-		else
-		{
-			yPos -= SPEED;
-			if (yPos < 0) {
-				movingUp = !movingUp;
-			}
-		}
-
-		//input Code
-		if (IsKeyDown(88) && !xKeyDown) {
-			movingRight = !movingRight;
-			xKeyDown = true;
-		}
-		else if (!IsKeyDown(88)) {
-			xKeyDown = false;
-		}
-
-
-		if (IsKeyDown(89) && !yKeyDown) {
-			movingUp = !movingUp;
-			yKeyDown = true;
-		}
-		else if (!IsKeyDown(89)) {
-			yKeyDown = false;
-		}
-
-		//draw/finalize move
-        MoveSprite(myTextureHandle, xPos, yPos);
-		DrawSprite(myTextureHandle);
-
+		rect.draw();
         ClearScreen();
 
     } while(!FrameworkUpdate());
